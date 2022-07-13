a340d03c241721acbed33613dffaf2f12d2145cf
isaack-mungui <munguiisaack@gmail.com>
2022-07-13 14:10:14 +0300

    Pushed changes to rust files

---
+++ gema-wallet/target/debug/deps/gema_wallet-a9fd21a6adf6a6a3.d
@@ -0,0 +1,5 @@
+/home/isaack/gema-wallet/gema-wallet/target/debug/deps/gema_wallet-a9fd21a6adf6a6a3.rmeta: src/main.rs
+
+/home/isaack/gema-wallet/gema-wallet/target/debug/deps/gema_wallet-a9fd21a6adf6a6a3.d: src/main.rs
+
+src/main.rs:
